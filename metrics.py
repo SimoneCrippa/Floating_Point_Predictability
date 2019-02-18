ea647796fb87c6d786b793916e45130dbc81c380
Alessio <alessio.cantina@gmail.com>
2019-02-18 22:47:32 +0100

    updated metrics script to improve automation

--- metrics.py
+++ metrics.py
@@ -8,36 +8,46 @@ from hurst import compute_Hc
 
 path = raw_input("Provide folder with benches to analyze")
 
-for filename in os.listdir(path):
-    if(".txt" in filename):
-        print "\n" + "\n" + "Analyzing " + filename + "\n"
-        with open(filename, "r") as input:
-            array = []
-            for line in input:
-                array.append(int(line))
-
-        print "minimum: " + str(min(array))
-        print "maximum: " + str(max(array))
-        print "average: " + str(numpy.mean(array))
-        print "variance: " + str(numpy.var(array))
-        print "standard deviation: " + str(statistics.stdev(array))
+file = open("results.txt", "w")
 
 
-        kpss_stat, p_value, lags, crit = stat.kpss(array) #https://www.statsmodels.org/dev/generated/statsmodels.tsa.stattools.kpss.html
-        print "KPSS: " + str(kpss_stat)
+for folder in os.listdir(path):
+    if("." not in folder):
+        for filename in os.listdir(path+"/"+folder):
+            if(".txt" in filename):
+                print "\n" + "\n" + "Analyzing " + folder + "/" + filename + "\n"
+                file.write("\n" + "\n" + "Analyzing " + folder + "/" + filename + "\n")
+                with open(path + "/" + folder + "/" + filename, "r") as input:
+                    array = []
+                    for line in input:
+                        array.append(int(line))
+
+                file.write("minimum: " + str(min(array)) + "\n")
+                file.write("maximum: " + str(max(array)) + "\n")
+                file.write("average: " + str(numpy.mean(array)) + "\n")
+                file.write("variance: " + str(numpy.var(array)) + "\n")
+                file.write("standard deviation: " + str(statistics.stdev(array)) + "\n")
 
 
-        start = 0
-        end = 100
-        max_bds = 0
-        while(end <= len(array)):
-            bds_stat, pvalue = stat.bds(array[start:end]) #https://www.statsmodels.org/dev/generated/statsmodels.tsa.stattools.bds.html
-            if(bds_stat > max_bds):
-                max_bds= bds_stat
-            start += 1
-            end += 1
-        print "BDS: " + str(max_bds)
+                kpss_stat, p_value, lags, crit = stat.kpss(array) #https://www.statsmodels.org/dev/generated/statsmodels.tsa.stattools.kpss.html
+                file.write("KPSS: " + str(kpss_stat) + "\n")
 
 
-        H, c, data = compute_Hc(array, kind='change', simplified=True) #https://pypi.org/project/hurst/
-        print "Hurst Exponent: " + str(H)
+                start = 0
+                end = 100
+                max_bds = 0
+                while(end <= len(array)):
+                    bds_stat, pvalue = stat.bds(array[start:end]) #https://www.statsmodels.org/dev/generated/statsmodels.tsa.stattools.bds.html
+                    if(bds_stat > max_bds):
+                        max_bds= bds_stat
+                    start += 1
+                    end += 1
+                file.write("BDS: " + str(max_bds) + "\n")
+
+
+                H, c, data = compute_Hc(array, kind='change', simplified=True) #https://pypi.org/project/hurst/
+                file.write("Hurst Exponent: " + str(H) + "\n")
+
+file.close()
+
+
