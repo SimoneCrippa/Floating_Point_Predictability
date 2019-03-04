9b5431a6797005f74321b5848d195510ef0c1116
SimoneCrippa <simone3.crippa@mail.polimi.it>
2019-03-04 12:04:24 +0100

    updated fixed_point_example.c to show the precision of fixed point operations

--- fixed_point_example.c
+++ fixed_point_example.c
@@ -8,14 +8,14 @@
 int main(){
 	int64_t scaling_factor = pow(2,SHIFT_AMOUNT);
 
-	int64_t fixed_point_value1 = (int64_t)(230.52 * scaling_factor);
-	int64_t fixed_point_value2 = (int64_t)(80.12 * scaling_factor);
+	int64_t fixed_point_value1 = (int64_t)(230.52111 * scaling_factor);
+	int64_t fixed_point_value2 = (int64_t)(80.12111 * scaling_factor);
 
-	float floating_point_value1 = ((float)fixed_mul_64(fixed_point_value1, fixed_point_value2, SHIFT_AMOUNT))/scaling_factor;
-	float floating_point_value2 = ((float)fixed_div_64(fixed_point_value1, fixed_point_value2, SHIFT_AMOUNT))/scaling_factor;
-	float floating_point_value3 = ((float)(fixed_point_value1 + fixed_point_value2))/scaling_factor;
-	float floating_point_value4 = ((float)(fixed_point_value1 - fixed_point_value2))/scaling_factor;
+	double floating_point_value1 = ((double)fixed_mul_64(fixed_point_value1, fixed_point_value2, SHIFT_AMOUNT))/scaling_factor;
+	double floating_point_value2 = ((double)fixed_div_64(fixed_point_value1, fixed_point_value2, SHIFT_AMOUNT))/scaling_factor;
+	double floating_point_value3 = ((double)(fixed_point_value1 + fixed_point_value2))/scaling_factor;
+	double floating_point_value4 = ((double)(fixed_point_value1 - fixed_point_value2))/scaling_factor;
 
-	printf("multiplication: %f\ndivision: %f\nsum: %f\nsubtraction: %f\n",floating_point_value1,floating_point_value2,floating_point_value3,floating_point_value4);
+	printf("multiplication: %.10f\ndivision: %.10f\nsum: %.10f\nsubtraction: %.10f\n",floating_point_value1,floating_point_value2,floating_point_value3,floating_point_value4);
 	return 0;
 }
